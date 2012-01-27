59790da4d1d335ff4fe9ee2ad6c5ee506f8f65f5
Marcus Kida <kimar@me.com>
2012-01-27 11:30:20 +0100

    Initial Commit

---
+++ Ragefac/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  Ragefac
+//
+//  Created by Kida Marcus on 27.01.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
